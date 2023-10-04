4cb2879b3dccc81e0da06d5d684c1981dd137f69
rawc <gmasontf2@gmail.com>
2023-10-04 01:51:38 -0400

    playlist color assoc w image, other various changes

--- app/static/js/playlist.js
+++ app/static/js/playlist.js
@@ -1,23 +1,48 @@
 document.addEventListener('DOMContentLoaded', function () {
+  const colorThief = new ColorThief();
   const playlistContainer = document.getElementById('playlist-container');
 
   playlistData.forEach(function (playlist) {
-    const collaborativeLabel = playlist.collaborative ? '& Collaborative' : '';
+    const img = new Image();
+    img.crossOrigin = 'anonymous';
+    img.src = playlist.cover_art;
 
-    const securityLabel = playlist.public ? 'Public' : 'Private';
+    img.onload = function () {
+      const palette = colorThief.getPalette(img, 3); // Get the 3 most dominant colors
 
-    playlistContainer.innerHTML += `
-  <div class="playlist-item">
-    <a href="/playlist/${playlist.id}?playlist_name=${encodeURIComponent(
-      playlist.name,
-    )}"class="playlist-option">
-      <div class="image-container">  
-        <img src="${playlist.cover_art}" alt="${
-          playlist.name
-        }" class="playlist-image">
-        <div class="overlay-text">${playlist.name}</div>
-      </div>
-    </a>
-  </div>`;
+      // If there are less than 3 dominant colors, fill the remaining spaces with grey
+      while (palette.length < 3) {
+        palette.push([128, 128, 128]); // RGB for grey
+      }
+
+      // Convert each color to RGB format and create a box-shadow for each
+      const boxShadow = palette
+        .map(
+          (color, index) =>
+            `${10 * (index + 1)}px ${10 * (index + 1)}px 20px rgb(${
+              color[0]
+            }, ${color[1]}, ${color[2]})`,
+        )
+        .join(', ');
+
+      const collaborativeLabel = playlist.collaborative
+        ? '& Collaborative'
+        : '';
+      const securityLabel = playlist.public ? 'Public' : 'Private';
+
+      playlistContainer.innerHTML += `
+        <div class="playlist-item">
+          <a href="/playlist/${playlist.id}?playlist_name=${encodeURIComponent(
+            playlist.name,
+          )}" class="playlist-option">
+            <div class="image-container" style="box-shadow: ${boxShadow};">
+              <img src="${playlist.cover_art}" alt="${
+                playlist.name
+              }" class="playlist-image">
+              <div class="overlay-text">${playlist.name}</div>
+            </div>
+          </a>
+        </div>`;
+    };
   });
 });
--- app/static/js/spec_playlist.js
+++ app/static/js/spec_playlist.js
@@ -1,128 +1,176 @@
-$('.data-view-btn').click(function () {
-  var btnId = $(this).attr('id');
+$(document).ready(function () {
+  const img = $('.playlist-cover')[0];
+  if (img) {
+    const colorThief = new ColorThief();
+    img.crossOrigin = 'anonymous';
+    img.onload = function () {
+      const color = colorThief.getColor(img);
 
-  var dataViewToShow;
-  switch (btnId) {
-    case 'summary-stats-btn':
-      dataViewToShow = 'summary-stats';
-      break;
-    case 'genre-counts-btn':
-      dataViewToShow = 'genre-counts';
-      break;
-    case 'feature-stats-btn':
-      dataViewToShow = 'feature-stats';
-      break;
+      const boxShadow = Array(5) // Create 5 layers of shadows for the aura effect
+        .fill()
+        .map(
+          (_, index) =>
+            `0 0 ${10 + index * 10}px ${10 + index * 5}px rgba(${color[0]}, ${
+              color[1]
+            }, ${color[2]}, 0.2)`,
+        )
+        .join(', ');
+
+      img.style.boxShadow = boxShadow;
+    };
+    img.src = img.src; // Trigger the onload event
   }
+  const artistContainers = $('.artist-container');
 
-  $('.data-view').hide();
+  // Loop over each container
+  artistContainers.each(function (index, container) {
+    const img = $(container).find('.artist-image')[0];
+    img.crossOrigin = 'anonymous';
+    img.onload = function () {
+      const colorThief = new ColorThief();
+      const color = colorThief.getColor(img);
 
-  // Show the selected data view
-  $('#' + dataViewToShow).show();
-});
-$('.data-view-btn').click(function () {
-  $('.data-view-btn').removeClass('active');
+      const boxShadow = Array(5) // Create 5 layers of shadows for the aura effect
+        .fill()
+        .map(
+          (_, index) =>
+            `0 0 ${10 + index * 10}px ${10 + index * 5}px rgba(${color[0]}, ${
+              color[1]
+            }, ${color[2]}, 0.2)`,
+        )
+        .join(', ');
 
-  $(this).addClass('active');
+      container.style.boxShadow = boxShadow;
+    };
+    img.src = img.src; // Trigger the onload event
+  });
 
-  var btnId = $(this).attr('id');
-  var dataViewToShow = btnId.replace('-btn', '');
+  $('.data-view-btn').click(function () {
+    var btnId = $(this).attr('id');
 
-  $('.data-view').hide();
+    var dataViewToShow;
+    switch (btnId) {
+      case 'summary-stats-btn':
+        dataViewToShow = 'summary-stats';
+        break;
+      case 'genre-counts-btn':
+        dataViewToShow = 'genre-counts';
+        break;
+      case 'feature-stats-btn':
+        dataViewToShow = 'feature-stats';
+        break;
+    }
 
-  $('#' + dataViewToShow).show();
-});
+    $('.data-view').hide();
 
-let labels = Object.keys(yearCountData);
-let data = Object.values(yearCountData);
+    // Show the selected data view
+    $('#' + dataViewToShow).show();
+  });
+  $('.data-view-btn').click(function () {
+    $('.data-view-btn').removeClass('active');
 
-var ctx = document.getElementById('YrPieChart').getContext('2d');
-var myPieChart = new Chart(ctx, {
-  type: 'pie',
-  data: {
-    labels: labels,
-    datasets: [
-      {
-        data: data,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.5)',
-          'rgba(54, 162, 235, 0.5)',
-          'rgba(255, 206, 86, 0.5)',
-          'rgba(75, 192, 192, 0.5)',
-          'rgba(153, 102, 255, 0.5)',
-          'rgba(255, 159, 64, 0.5)',
-        ],
-      },
-    ],
-  },
-  options: {
-    cutoutPercentage: 5, // Reduce the pie size slightly
-    responsive: true,
-    plugins: {
-      title: {
-        display: false,
-        text: 'Distribution of Track Release Dates',
-        fontSize: 16, // Optional: Adjust font size as needed
-        align: 'center',
-        position: 'bottom',
-      },
-      legend: {
-        position: 'left', // Position the legend on the right side of the chart
-        labels: {
-          boxWidth: 10, // Optional: Adjust box width of the legend color boxes
-          padding: 5, // Optional: Adjust padding between legend items
+    $(this).addClass('active');
+
+    var btnId = $(this).attr('id');
+    var dataViewToShow = btnId.replace('-btn', '');
+
+    $('.data-view').hide();
+
+    $('#' + dataViewToShow).show();
+  });
+
+  let labels = Object.keys(yearCountData);
+  let data = Object.values(yearCountData);
+
+  var ctx = document.getElementById('YrPieChart').getContext('2d');
+  var myPieChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          data: data,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.5)',
+            'rgba(54, 162, 235, 0.5)',
+            'rgba(255, 206, 86, 0.5)',
+            'rgba(75, 192, 192, 0.5)',
+            'rgba(153, 102, 255, 0.5)',
+            'rgba(255, 159, 64, 0.5)',
+          ],
+        },
+      ],
+    },
+    options: {
+      cutoutPercentage: 5, // Reduce the pie size slightly
+      responsive: true,
+      plugins: {
+        title: {
+          display: false,
+          text: 'Distribution of Track Release Dates',
+          fontSize: 16, // Optional: Adjust font size as needed
+          align: 'center',
+          position: 'bottom',
+        },
+        legend: {
+          position: 'left', // Position the legend on the right side of the chart
+          labels: {
+            boxWidth: 10, // Optional: Adjust box width of the legend color boxes
+            padding: 5, // Optional: Adjust padding between legend items
+          },
         },
       },
     },
-  },
-});
-
-$('#like-all-songs-btn').click(function () {
-  $.get('/like_all_songs/' + playlistId, function (response) {
-    showToast(response); // Assumes a successful response message
-  }).fail(function (error) {
-    showToast('An error occurred while liking all songs.', 'error');
   });
-});
 
-$('#unlike-all-songs-btn').click(function () {
-  $.get('/unlike_all_songs/' + playlistId, function (response) {
-    showToast(response);
-  }).fail(function (error) {
-    showToast('An error occurred while unliking all songs.', 'error');
+  $('#like-all-songs-btn').click(function () {
+    $.get('/like_all_songs/' + playlistId, function (response) {
+      showToast(response); // Assumes a successful response message
+    }).fail(function (error) {
+      showToast('An error occurred while liking all songs.', 'error');
+    });
   });
-});
 
-$('#remove-duplicates-btn').click(function () {
-  $.get('/remove_duplicates/' + playlistId, function (response) {
-    showToast('Successfully removed duplicates.');
-  }).fail(function (error) {
-    showToast('An error occurred while removing duplicates.', 'error');
+  $('#unlike-all-songs-btn').click(function () {
+    $.get('/unlike_all_songs/' + playlistId, function (response) {
+      showToast(response);
+    }).fail(function (error) {
+      showToast('An error occurred while unliking all songs.', 'error');
+    });
   });
-});
 
-function showToast(message, type = 'success') {
-  const toast = document.getElementById('toast');
-  const toastMessage = document.getElementById('toastMessage');
+  $('#remove-duplicates-btn').click(function () {
+    $.get('/remove_duplicates/' + playlistId, function (response) {
+      showToast('Successfully removed duplicates.');
+    }).fail(function (error) {
+      showToast('An error occurred while removing duplicates.', 'error');
+    });
+  });
 
-  toastMessage.textContent = message;
+  function showToast(message, type = 'success') {
+    const toast = document.getElementById('toast');
+    const toastMessage = document.getElementById('toastMessage');
 
-  if (type === 'error') {
-    toast.classList.add('error');
-    toast.classList.remove('success');
-  } else {
-    toast.classList.add('success');
-    toast.classList.remove('error');
+    toastMessage.textContent = message;
+
+    if (type === 'error') {
+      toast.classList.add('error');
+      toast.classList.remove('success');
+    } else {
+      toast.classList.add('success');
+      toast.classList.remove('error');
+    }
+
+    toast.style.display = 'block';
+
+    // Automatically hide the toast after 5 seconds
+    setTimeout(() => {
+      toast.style.display = 'none';
+    }, 5000);
   }
 
-  toast.style.display = 'block';
-
-  // Automatically hide the toast after 5 seconds
-  setTimeout(() => {
-    toast.style.display = 'none';
-  }, 5000);
-}
-
-// Closing the toast when the 'X' is clicked
-document.querySelector('.close-toast').addEventListener('click', function () {
-  this.parentElement.style.display = 'none';
+  // Closing the toast when the 'X' is clicked
+  document.querySelector('.close-toast').addEventListener('click', function () {
+    this.parentElement.style.display = 'none';
+  });
 });
