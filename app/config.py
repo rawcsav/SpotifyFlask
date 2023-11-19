36906f06cfd2ccd1a7ed0e52cb3576c417179a40
rawc <gmasontf2@gmail.com>
2023-11-19 10:59:33 -0500

    landing change w img/font, config change

--- app/config.py
+++ app/config.py
@@ -1,11 +1,6 @@
 import os
 from datetime import timedelta
 
-if os.getenv('FLASK_ENV') == "development":
-    from dotenv import load_dotenv
-
-    load_dotenv()
-
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 REDIRECT_URI = os.getenv("REDIRECT_URI")
